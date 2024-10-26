95ea5c9549a7e8b46fe65e69166fa093af69a25b
pcfll <semenations@gmail.com>
2024-10-26 17:25:14 +0500

    выполнил задания второй главы

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?
-print_r('привет Егор');
+print_r('ЗАДАНИЯ 2 ГЛАВЫ ');
+print_r('<hr>');
+print_r('1 ');
+print_r('Hello World ');
+print_r('2 ');
+print_r('Sublime Text установлен и выполнена задача через него ');
+print_r('3 ');
+print_r('echo выводит строки');
+//phpinfo();
+print_r('4');
+print_r('я думаю, что нахуй не нужен этот VirtualBox и альтернативная система');
+print_r('5');
+print_r('Web сервер nginx также, я так понял, не нужен');
+print_r('<h1> 123123 </h1>');
 ?>
