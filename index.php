9a4f485927a7eeb2183ce707f9de628f63b848cf
Песяннеков Александр <Egor>
2024-10-26 17:55:55 +0500

    Аххахаах

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?
 print_r('ЗАДАНИЯ 2 ГЛАВЫ ');
 print_r('<hr>');
-print_r('1 ');
+print_r('1 Задание');
 print_r('Hello World ');
 print_r('<hr>');
-print_r('2 ');
+print_r('2 Задание');
 print_r('Sublime Text установлен и выполнена задача через него ');
 print_r('<hr>');
-print_r('3 ');
+print_r('3 Задание');
 print_r('echo выводит строки ');
 print_r('<hr>');
 //phpinfo();
-print_r('4 ');
+print_r('4 Задание');
 print_r('я думаю, что нахуй не нужен этот VirtualBox и альтернативная система ');
 print_r('<hr>');
-print_r('5 ');
+print_r('5 Задание');
 print_r('Web сервер nginx также, я так понял, не нужен');
 ?>
